18a2ae1749c05b8445fcabd5be77838b30d15ad3
Wei Ye <weiye@cs.ucsb.edu>
2021-06-08 22:55:03 +0800

    Add files via upload

--- fuse.m
+++ fuse.m
@@ -9,12 +9,13 @@ function [meanValue,stdDev]=fuse(A,k,label)
 
 baseconv=1e-5;
 p=k+1;
-for run=1:50
+
+parfor run=1:50
     [E,pseudoeigen,whitened,mutualInfo]=eigenvectorFuse(A,p,baseconv);
     kur=kurtosis(pseudoeigen);
     [~,b]=sort(kur);
     v=pseudoeigen(:,b(1:k));
-    
+    tmpami=[];
     for i=1:100
         C = kmeans(v,k);
         [~,AMI,~,~]=ANMI_analytical_11(label,C);
@@ -22,11 +23,11 @@ for run=1:50
     end
     tmpami=round(100*tmpami)/100;
     unitmp=unique(tmpami);
+    num=[];
     for i=1:size(unitmp,2)
-        num(i)=size(find(tmpami==unitmp(i)),1);
+        num(i)=length(find(tmpami==unitmp(i)));
     end
     [~,c]=sort(num);
-    index=find(tmpami==unitmp(c(end)));
     resultami(run,1)=unitmp(c(end));
 end
 
