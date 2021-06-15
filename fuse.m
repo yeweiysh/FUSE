03be844ee7c7f7a676fda64237bfedb4c747ec7d
Wei Ye <weiye@cs.ucsb.edu>
2021-06-15 15:44:48 +0800

    Add files via upload

--- fuse.m
+++ fuse.m
@@ -6,30 +6,43 @@ function [meanValue,stdDev]=fuse(A,k,label)
 % meanValue: the mean value of AMI
 % stdDev: the standard deviation of AMI
 
+unilabel = unique(label);
+newlabel = [];
+l=1;
+for i=1:length(unilabel)
+    index = find(label==unilabel(i));
+    newlabel(index,1)=l;
+    l = l+1;
+end
 
 baseconv=1e-5;
 p=k+1;
 
-parfor run=1:50
+for run=1:50
     [E,pseudoeigen,whitened,mutualInfo]=eigenvectorFuse(A,p,baseconv);
     kur=kurtosis(pseudoeigen);
     [~,b]=sort(kur);
     v=pseudoeigen(:,b(1:k));
-    tmpami=[];
+    nmi=[];
+    ami=[];
     for i=1:100
         C = kmeans(v,k);
-        [~,AMI,~,~]=ANMI_analytical_11(label,C);
-        tmpami(i)=AMI;
+        [NMI1,AMI1,~,~]=ANMI_analytical_11(newlabel,C);
+        nmi(i)=NMI1;
+        ami(i)=AMI1;
     end
-    tmpami=round(100*tmpami)/100;
-    unitmp=unique(tmpami);
+    nmi=round(100*nmi)/100;
+    ami=round(100*ami)/100;
+    uni_nmi=unique(nmi);
     num=[];
-    for i=1:size(unitmp,2)
-        num(i)=length(find(tmpami==unitmp(i)));
+    for i=1:size(uni_nmi,2)
+        num(i)=length(find(nmi==uni_nmi(i)));
     end
     [~,c]=sort(num);
-    resultami(run,1)=unitmp(c(end));
+    ind=find(nmi==uni_nmi(c(end)));
+    result_nmi(run)=nmi(ind(1));
+    result_ami(run)=ami(ind(1));
 end
 
-meanValue=mean(resultami);
-stdDev=std(resultami);
+meanValue=mean(result_ami);
+stdDev=std(result_ami);
